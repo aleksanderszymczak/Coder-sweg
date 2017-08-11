d70aa43fad35727cb73e7dabdb98f7e998ac27e8
Aleksander <aleksanderszymczak@gmail.com>
2017-08-11 17:39:42 +0200

    collection view end

---
+++ Coder-sweg/View/ProductCell.swift
@@ -0,0 +1,22 @@
+//
+//  ProductCell.swift
+//  Coder-sweg
+//
+//  Created by olos on 11.08.2017.
+//  Copyright © 2017 olos. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+    
+    
+    
+}
